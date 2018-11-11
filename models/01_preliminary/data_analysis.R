df36602cdfd32d141dd9c146cd11f5caade4a4d8
Eric Massip <ericmassip1@gmail.com>
2018-11-11 23:33:06 +0100

    Removed redundant comments in data_analysis.R

--- models/01_preliminary/data_analysis.R
+++ models/01_preliminary/data_analysis.R
@@ -11,11 +11,11 @@ fallacies.df <- read.csv(file=file.choose(), header=TRUE, sep="\t")
 cleaned_fallacies.df <- data.frame(fallacies.df$Topic, fallacies.df$Intended.Fallacy, fallacies.df$Text)
 cleaned_fallacies.df <- cleaned_fallacies.df[-which((cleaned_fallacies.df$fallacies.df.Topic == "") | (cleaned_fallacies.df$fallacies.df.Intended.Fallacy == "") | (cleaned_fallacies.df$fallacies.df.Text == "")),]
 
+cleaned_fallacies_sorted.df <- cleaned_fallacies.df[with(cleaned_fallacies.df, order(cleaned_fallacies.df$fallacies.df.Topic)),]
 
-ad_hominem_attacks.sub <- cleaned_fallacies.df[grep("Ad Hominem", cleaned_fallacies.df$fallacies.df.Intended.Fallacy),]
-ad_hominem_attacks.sub <- mutate(ad_hominem_attacks.sub, NegativeExample = "")
+cleaned_fallacies_sorted.df <- cleaned_fallacies_sorted.df[which((cleaned_fallacies_sorted.df$fallacies.df.Intended.Fallacy=="Ad Hominem") | cleaned_fallacies_sorted.df$fallacies.df.Intended.Fallacy=="No Fallacy"),]
+cleaned_fallacies_sorted.df <- mutate(cleaned_fallacies_sorted.df, Eric = "")
+cleaned_fallacies_sorted.df <- mutate(cleaned_fallacies_sorted.df, Pieter = "")
+cleaned_fallacies_sorted.df <- mutate(cleaned_fallacies_sorted.df, Murilo = "")
 
-non_fallacies.sub <- cleaned_fallacies.df[grep("No Fallacy", cleaned_fallacies.df$fallacies.df.Intended.Fallacy),]
-
-write.csv(non_fallacies.sub, "~/Projects/MAI/1st_semester/KTW/G0B34a_knowledge_and_the_web/data/general/non_fallacies.csv")
-write.csv(ad_hominem_attacks.sub, "~/Projects/MAI/1st_semester/KTW/G0B34a_knowledge_and_the_web/data/general/ad_hominem_attacks.csv")
+write_delim(cleaned_fallacies_sorted.df, "~/Projects/MAI/1st_semester/KTW/G0B34a_knowledge_and_the_web/data/general/ad_hominem_attacks.csv", delim = ",")
