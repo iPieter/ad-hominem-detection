bc99f9dfcd039ae5f4e3d79979bd8a50ad2ac69d
Eric Massip <ericmassip1@gmail.com>
2018-11-29 20:49:31 +0100

    Skipped N-grams developed

--- models/02_tfidf/helper.py
+++ models/02_tfidf/helper.py
@@ -1,15 +1,11 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
+from nltk.util import skipgrams
 import pandas as pd
-
-
-# In[9]:
-
 
 def get_data():
     return pd.read_csv('../../data/merged_datasets.csv', usecols=(['body', 'isAdHominem']))
 
+def get_data_with_skip_n_grams(n, k):
+    data = get_data()
+    for index, row in data.iterrows():
+        data.at[index, 'body'] = list(skipgrams(data.loc[index, 'body'].split(), n, k))
+    return data
